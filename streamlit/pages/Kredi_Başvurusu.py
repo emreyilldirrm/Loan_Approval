8f8c1fc32d38a8ed211d68493c61e3da635bcc35
emreyilldirrm <emreyl.yldrm@yandex.com>
2025-04-21 00:24:09 +0300

    “update”

--- streamlit/pages/Kredi_Başvurusu.py
+++ streamlit/pages/Kredi_Başvurusu.py
@@ -7,8 +7,6 @@ model_path = os.path.join(os.path.dirname(__file__), "..", "model_with_preproces
 model_path = os.path.abspath(model_path)
 st.write(model_path)
 
-model_path = os.path.join(os.path.dirname(__file__))
-model_path = os.path.abspath(model_path)
 st.write(os.listdir(model_path))
 st.write(model_path)
 # 🎨 Sayfa ayarları
