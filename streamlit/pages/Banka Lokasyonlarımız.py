728ada04fcbbb73594a8658b91ec773c50c73d8d
emreyilldirrm <emreyl.yldrm@yandex.com>
2025-04-20 23:16:59 +0300

    first commit

---
+++ streamlit/pages/Banka Lokasyonlarımız.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+df=pd.read_json("https://raw.githubusercontent.com/mmcloughlin/starbucks/master/locations.json")
+ulkeler=df['country'].unique()
+ulke=st.sidebar.selectbox("Ülke Seç",ulkeler)
+df=df[df['country']==ulke]
+sehirler=df['city'].unique()
+sehir=st.sidebar.selectbox("Şehir Seç",sehirler)
+df=df[df['city']==sehir]
+
+st.header("Banka Lokasyonlarımız")
+st.map(df)
+
