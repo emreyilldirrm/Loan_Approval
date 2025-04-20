abf3df922307d39b5a1c5fe3da0611ca6c14dd61
emreyilldirrm <emreyl.yldrm@yandex.com>
2025-04-21 00:24:48 +0300

    “update”

--- streamlit/pages/Kredi_Başvurusu.py
+++ streamlit/pages/Kredi_Başvurusu.py
@@ -4,10 +4,11 @@ import streamlit as st
 import os
 
 model_path = os.path.join(os.path.dirname(__file__), "..", "model_with_preprocessor.pkl")
+st.write(os.listdir(model_path))
 model_path = os.path.abspath(model_path)
 st.write(model_path)
 
-st.write(os.listdir(model_path))
+
 st.write(model_path)
 # 🎨 Sayfa ayarları
 st.set_page_config(
